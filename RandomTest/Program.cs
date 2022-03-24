d6c9aed8302b89c0d8fe8b71ea751a1b7821c8e5
Pedroosorio97 <pedromrnosorio97@gmail.com>
2022-03-24 12:45:36 +0000

    Testing values for Project

--- RandomTest/Program.cs
+++ RandomTest/Program.cs
@@ -6,13 +6,13 @@ namespace RandomTest
     {
         private static void Main(string[] args)
         {
-            Random r1 = new Random(22);
-            Random r2 = new Random(22);
+            Random r1 = new Random();
+            Random r2 = new Random();
             for (int i = 0; i < 4; i++)
-                Console.WriteLine(r1.Next());
+                Console.WriteLine(r1.Next(1,7));
             Console.WriteLine();
             for (int i = 0; i < 4; i++)
-                Console.WriteLine(r2.Next());
+                Console.WriteLine(r2.Next(1,7));
         }
     }
 }
