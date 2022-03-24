9c91ef6924beec4f77cc21331b8075123f79b321
Pedroosorio97 <pedromrnosorio97@gmail.com>
2022-03-24 12:09:02 +0000

    Both stopwatches runtimes working and showing on console

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Chronos
 {
@@ -6,7 +8,26 @@ namespace Chronos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declaration of instance of classes
+            Stopwatch cron1 = new Stopwatch();
+            Stopwatch cron2 = new Stopwatch();
+
+            //stopwatch 1 begins counting
+            cron1.Start();
+            //Program enters pause
+            Thread.Sleep(1000);
+            //stopwatch 2 begins counting
+            cron2.Start();
+            //Program enters pause
+            Thread.Sleep(1000);
+            //Stopwatch 2 Stops
+            cron2.Stop();
+            //StopWatch 1 Stops
+            cron1.Stop();
+            
+            //Printing of the Stopwatches time
+            Console.WriteLine($"StopWatch 1 Runtime in Miliseconds: {cron1.ElapsedMilliseconds}");
+            Console.WriteLine($"Stopwatch 2 Runtime in Miliseconds:{cron2.ElapsedMilliseconds}");
         }
     }
 }
