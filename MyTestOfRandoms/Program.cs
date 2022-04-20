589f014499e2683c862f6f257ce10008def5256a
Pedroosorio97 <pedromrnosorio97@gmail.com>
2022-04-20 15:43:06 +0100

    Result of the sum of dice numbers showing

--- MyTestOfRandoms/Program.cs
+++ MyTestOfRandoms/Program.cs
@@ -14,11 +14,19 @@ namespace MyTestOfRandoms
             Console.WriteLine("Write a number for how many dices you want to roll");
             int numberOfDiceRolls = int.Parse(Console.ReadLine());
             Random r1 = new Random();
-            
+            int diceNumber = 0;
+            int diceNumberSum = 0;
             for (int i = 0; i < numberOfDiceRolls; i++)
             {
-                Console.WriteLine(r1.Next(1,7));
+                diceNumber = r1.Next(1,7);
+                Console.WriteLine($"Dice Number gotten: {diceNumber}");
+                diceNumberSum = diceNumberSum + diceNumber; 
             }
+
+            Console.WriteLine($"The Total of the dice numbers gotten is: {diceNumberSum}");
+
+
+
                 
             
         }
