fa70e027fce6f498c424d30a9fb55ecd8fdfd817
Pedroosorio97 <pedromrnosorio97@gmail.com>
2022-04-20 15:33:13 +0100

    A Number of dices roll depending on the user input

--- MyTestOfRandoms/Program.cs
+++ MyTestOfRandoms/Program.cs
@@ -6,7 +6,22 @@ namespace MyTestOfRandoms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RollDices();
         }
+
+        private static void RollDices()
+        {
+            Console.WriteLine("Write a number for how many dices you want to roll");
+            int numberOfDiceRolls = int.Parse(Console.ReadLine());
+            Random r1 = new Random();
+            
+            for (int i = 0; i < numberOfDiceRolls; i++)
+            {
+                Console.WriteLine(r1.Next(1,7));
+            }
+                
+            
+        }
+
     }
 }
